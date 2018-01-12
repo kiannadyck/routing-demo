c28cc5673854e0aa06565e16fc433cf50543e1bf
Kianna Dyck <kdyck@mail.greenriver.edu>
2018-01-12 14:49:57 -0500

    Added routes

--- index.php
+++ index.php
@@ -5,12 +5,27 @@ require_once('vendor/autoload.php');
 // Create an instance of the Base class
 $f3 = Base::instance();
 
+// Set debug level 0 = off, 3 = max level
+$f3->set('DEBUG', 3);
+
 // Define a default route
 $f3->route('GET /', function() {
     echo '<h1>Routing Demo</h1>';
-    //    $view = new View();
-    //    echo $view->render
-    //    ('views/home.html');
+});
+
+// Define a page1 route
+$f3->route('GET /page1', function() {
+    echo '<h1>This is page1</h1>';
+});
+
+// Define a page1 route
+$f3->route('GET /page1/subpage1', function() {
+    echo '<h1>This is page1, subpage1</h1>';
+});
+
+// Define a page2 route
+$f3->route('GET /page2', function() {
+    echo '<h1>This is page2</h1>';
 });
 
 // Run fat free
