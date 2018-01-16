e340f554301a66cc57a90e4dfdba13c5d1ea2662
Kianna Dyck <kdyck@mail.greenriver.edu>
2018-01-16 13:43:41 -0500

    Added language route

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ $f3->route('GET /page1', function() {
     echo '<h1>This is page1</h1>';
 });
 
-// Define a page1 route
+// Define a page1 subpage route
 $f3->route('GET /page1/subpage1', function() {
     echo '<h1>This is page1, subpage1</h1>';
 });
@@ -26,6 +26,34 @@ $f3->route('GET /page1/subpage1', function() {
 // Define a page2 route
 $f3->route('GET /page2', function() {
     echo '<h1>This is page2</h1>';
+});
+
+// practice instantiating a template for a route
+$f3->route('GET /jewelry/rings/toe-rings', function() {
+//    echo '<h1>Toe-Rings</h1>';
+    $template = new Template();
+    echo $template->render('views/toe-rings.html');
+});
+
+// practice using tokens; need the f3 object and params array passed to anonymous function
+$f3->route('GET /hello/@name', function($f3, $params) {
+    $name = $params['name'];
+    echo "<h1>Hello, $name</h1>";
+});
+
+$f3->route('GET /language/@lang', function($f3, $params) {
+    switch($params['lang']) {
+        case 'swahili' :
+            echo 'Jumbo!'; break;
+        case 'spanish' :
+            echo 'Hola!'; break;
+        case 'russian':
+            echo 'Privit!'; break;
+        case 'farsi' :
+            echo 'Salam!'; break;
+        default:
+            echo 'Hello!';
+    }
 });
 
 // Run fat free
